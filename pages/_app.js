6c98592ad91e6382e6b042983c8f38fd234b5026
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-12-26 15:44:58 +0700

    -Custom document for html

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "../apollo/config/Apollo";
 import { Navbar } from "../components";
 import { Context } from "../context";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [myPokemon, setMyPokemon] = useState([]);
@@ -18,12 +19,27 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <ApolloProvider client={client}>
-      <Context.Provider value={providerValue}>
-        <Navbar />
-        <Component {...pageProps} />
-      </Context.Provider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <meta name="description" content="Pokédex app for pokémon lovers." />
+        <meta property="og:title" content="Pokédex App" />
+        <meta
+          property="og:url"
+          content="http://https://pokedex-sepia-three.vercel.app//"
+        />
+        <meta
+          property="og:description"
+          content="Pokédex app for pokémon lovers."
+        />
+        <title>Pokédex</title>
+      </Head>
+      <ApolloProvider client={client}>
+        <Context.Provider value={providerValue}>
+          <Navbar />
+          <Component {...pageProps} />
+        </Context.Provider>
+      </ApolloProvider>
+    </>
   );
 }
 
