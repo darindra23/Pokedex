01cb2a524fce7b14cc272a964840bc5899163026
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-12-26 12:24:11 +0700

    -Infinite scroll to pokemons page

--- apollo/config/Apollo.js
+++ apollo/config/Apollo.js
@@ -2,5 +2,23 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "https://graphql-pokeapi.vercel.app/api/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          pokemons: {
+            keyArgs: false,
+            merge(existing, incoming) {
+              return existing
+                ? {
+                    ...existing,
+                    results: [...existing.results, ...incoming.results],
+                  }
+                : incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
 });
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
 import { useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
 import { useBottomScrollListener } from "react-bottom-scroll-listener";
 import { GET_POKEMONS } from "../apollo/query/GET_POKEMONS";
 import { Loading } from "../components";
@@ -9,41 +8,27 @@ const Main = dynamic(import("../components/Main"));
 const Pokemons = dynamic(import("../Content/Pokemons"));
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [variables, setVariables] = useState({ limit: 39, offset: 1 });
-  const [pokemons, setPokemons] = useState([]);
-
-  const { data, fetchMore } = useQuery(GET_POKEMONS, {
-    variables: { limit: variables.limit, offset: variables.offset },
+  const { loading, data, fetchMore } = useQuery(GET_POKEMONS, {
+    variables: {
+      limit: 39,
+      offset: 1,
+    },
   });
 
   useBottomScrollListener(() => {
-    setLoading(true);
     fetchMore({
       variables: {
-        limit: variables.limit,
-        offset: variables.offset + variables.limit + 1,
+        offset: data.pokemons.results.length + 1,
       },
-    }).then(() => {
-      setVariables({
-        ...variables,
-        offset: variables.offset + variables.limit + 1,
-      });
+      notifyOnNetworkStatusChange: true,
     });
   });
-
-  useEffect(() => {
-    if (data) {
-      setPokemons([...pokemons, ...data.pokemons.results]);
-      setLoading(false);
-    }
-  }, [data]);
 
   return (
     <Main>
       {loading && <Loading />}
-      {pokemons?.map((x) => {
-        return <Pokemons key={x.id} data={x} />;
+      {data?.pokemons.results.map((x, idx) => {
+        return <Pokemons key={idx} data={x} />;
       })}
     </Main>
   );
